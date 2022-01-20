98236c9b3cf1175b78a49a65abeda575b44b51dd
ms <altjsvnf@naver.com>
2022-01-20 21:24:26 +0900

    메뉴 가운데정렬 문제 해결

---
+++ main.js
@@ -0,0 +1,11 @@
+const toggleBtn = document.querySelector('.navbar_toogleBtn');
+const menu = document.querySelector('.navbar_menu');
+const theme_change = document.querySelector('.theme_change');
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
+theme_change.addEventListener('click', () => {
+    theme_change.classList.toggle('active');
+});
